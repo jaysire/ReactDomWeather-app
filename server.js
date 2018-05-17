1425a5b5ec0934b8c0bec707b7593772a752fbbb
Josiah Maina <josiah5115@gmail.com>
2018-05-16 21:09:51 -0600

    dependent files

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express') // nodes key word require.
+
+// create our app
+var app = express()
+
+// tell it which folder we want to serve. helps us add functionality to our app
+var static = express.static('public');
+app.use(static);
+
+const PORT = 3000;
+app.listen(PORT, function () {
+  console.log('Express server is up on port' + ' ' + PORT);
+});
