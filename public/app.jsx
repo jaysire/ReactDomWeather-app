5e3e9f803fc6de3821d03aff123cba5679bdb325
Josiah Maina <josiah5115@gmail.com>
2018-05-18 01:56:02 -0600

    set initial state in component

--- public/app.jsx
+++ public/app.jsx
@@ -2,33 +2,41 @@ var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
-      message: 'This is the defaule message  that should appear when no props are passed through the component.'
+      message: 'This is the defaule message  that appears when no props are passed through the component.'
     }
   },
-
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
   onButtonClick: function (e) {
     e.preventDefault()
 
-    var name = this.refs.name.value
-
-    this.setState({
-      name: name
-    });
-
-    render: function () {
-    var name = this.props.name
+    var nameRef = this.refs.name
+    var name = nameRef.value
+    nameRef.value = ''
+    
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      })
+    }
+    },
+    
+  render: function () {
+    var name = this.state.name
     var message = this.props.message
-    return (
- 
-      <div>
 
-        < h1 > Hello {name}!</h1>
+    return (
+      <div>
+        <h1> Hello {name}! </h1>
         <p>{message + '!!'}</p>
         <p>Learning coding is fun and rewarding!< br /> Ras Joh ! Yoh !</p>
 
         <form onSubmit={this.onButtonClick}>
 
-          <input type="text" ref="name"/>
+          <input type="text" ref="name"/>,
 
           <button>Set Name</button>
 
@@ -38,10 +46,9 @@ var Greeter = React.createClass({
     )
   }
 })
-
-var firstName = 'RasJoh';
+var firstName = 'JohSire';
 var message = 'This is a paragraph made up of words, symbols, character and more.'
 
-ReactDOM.render(<Greeter name={firstName} message={message}/>,
-  document.getElementById('app')
-)
+ReactDOM.render(
+  <Greeter name={firstName} message={message}/>,
+  document.getElementById('app'))
