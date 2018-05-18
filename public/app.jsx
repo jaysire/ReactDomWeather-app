bf6c80b6f0b5d118a6a2fd921dbd25118af91a4d
Josiah Maina <josiah5115@gmail.com>
2018-05-18 04:02:41 -0600

    update comp challange

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,28 @@
-var Greeter = React.createClass({
+
+const GreeterText = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>First Try Component!</h1>
+        <p>This is the first paragraph in my component</p>
+
+      </div>
+    )
+  }
+})
+const GreeterMessage = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Some H1 Titile</h1>
+        <p>Some Paragraph</p>
+
+      </div>
+    )
+  }
+})
+
+const Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
@@ -13,30 +37,49 @@ var Greeter = React.createClass({
   onButtonClick: function (e) {
     e.preventDefault()
 
-    var nameRef = this.refs.name
-    var name = nameRef.value
+    const
+      nameRef = this.refs.name
+    const
+      name = nameRef.value
     nameRef.value = ''
-    
+
     if (typeof name === 'string' && name.length > 0) {
       this.setState({
         name: name
       })
     }
-    },
-    
+  },
+
   render: function () {
-    var name = this.state.name
-    var message = this.props.message
+    const
+      name = this.state.name
+    const
+      message = this.props.message
 
     return (
       <div>
+
         <h1> Hello {name}! </h1>
+
         <p>{message + '!!'}</p>
+
         <p>Learning coding is fun and rewarding!< br /> Ras Joh ! Yoh !</p>
+
+        <GreeterText/>
+
+        <GreeterMessage/>
+
+        <form>
+
+          <input type='text' ref='name'/>
+
+          <button>Name Here</button>
+
+        </form>
 
         <form onSubmit={this.onButtonClick}>
 
-          <input type="text" ref="name"/>,
+          <input type="text" ref="name"/>
 
           <button>Set Name</button>
 
@@ -46,8 +89,8 @@ var Greeter = React.createClass({
     )
   }
 })
-var firstName = 'JohSire';
-var message = 'This is a paragraph made up of words, symbols, character and more.'
+const firstName = 'JohSire'
+const message = 'This is a paragraph made up of words, symbols, character and more.'
 
 ReactDOM.render(
   <Greeter name={firstName} message={message}/>,
