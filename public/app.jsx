b4e8151d31384ae5b046ad10b9f1125c9104a998
JaySire <josiah5115@gmail.com>
2018-05-28 17:11:52 -0600

    -pass msg prop thru greeter class

--- public/app.jsx
+++ public/app.jsx
@@ -2,23 +2,34 @@ var Greeter = React.createClass({
   // we added a defaul anonymous func so it displays the mesge on the browser when none is set.
   getDefaultProps: function () {
     return {
-      name: 'React'
+      name: 'React',
+      message: 'This is our default message! Boooyyyy!'
+      // message: 'This is the default message!!'
     }
   },
   render: function () {
     var name = this.props.name
+    var message = this.props.message
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is from the Component!!<br/>...and then one day I got sucked into a warmhole ...<br/>Consequences Boooyyyyy!! (Key&Peele)</p>
+        <p>{message + '!<==!'}</p>
+
+
+        {/* <p>This is from the first Component!<br/><br/>...and then one day I got sucked into a warmhole! ...<br/>Consequences Boooyyyyy!! (Key&Peele).</p>
+
+        <h2>YOLO {message}!</h2>
+        <p>Message from the second Component! <br/><br/>...and then one day, a piano fell on my head!... <br/> Consequences Boooyyyyy!! (Key&Peele).</p> */}
 
       </div>
     )
   }
 })
 
+var firstName = 'Ras Joh' // You can pass any kind of data in the component, not just strings.
+
 ReactDOM.render(
-  <Greeter name='Josire'/>,
+  <Greeter name= {firstName}/>,
   document.getElementById('app')
 )
 
