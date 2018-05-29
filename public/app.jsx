89f1048f4c11aefc939f5ba383b85b76b5593edc
JaySire <josiah5115@gmail.com>
2018-05-28 21:47:49 -0600

    interactive set state

--- public/app.jsx
+++ public/app.jsx
@@ -6,15 +6,30 @@ var Greeter = React.createClass({
       message: 'This is the default message!'
     }
   },
+
+  // React inbuilt method w/ an anonymous func that does not need to take any arguments. it returns an obj that gets set to 'this'dot state very similar to set default props as seen above.
+
+  // ** Since a Comp is only allowed to maintain/change its state we create the above state where the user will input data/info and Comp will capture the data and render it out/ update our website to reflect the new input info from the client/user.
+
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
   onButtonClick: function (e) {
     e.preventDefault()
 
     var name = this.refs.name.value
 
-    alert(name)
+    // for React to re-render the Comp we need to call set State: 1) so we can set a new value for 'name' 2). So we can re-render our Component if it depends on the 'name' State; which it does in our example/case.
+
+    // set state takes an obj and only sets the state for the attributes that you provide, and in our case we only have 'name' that is set to the 'name' variable initialized above.
+    this.setState({
+      name: name
+    })
   },
   render: function () {
-    var name = this.props.name
+    var name = this.state.name
     var message = this.props.message
 
     return (
@@ -42,3 +57,9 @@ ReactDOM.render(
 
 // There's two types of data in a component, Props and State. Props get passed into the Component as you initialize it. e.g we initilized 'Greeter' with the name property(prop). While State is internally mainteined and updated by the component.
 // *** A Component shouldn't Uupdate its own Props but it is allowed to Update its own State(render out the updated/changed parts of the code to the website/monitor).
+
+// ** Data Flow Break-Down:
+// 1). Props get defined, as either default to the one passed in or React.
+// 2). Get Initial State, sets its name state to the Props value
+// 3). Down in the render func we use this.dot name to fetch (we initialize and store it in the name variable and call it down in the 'ref' of the form input section) the value and we display it to the screen using the form and input functionalities.
+// 4). When a user submits the form, we call this dot state which updates the state and re-render the parts of the Component that matter.
