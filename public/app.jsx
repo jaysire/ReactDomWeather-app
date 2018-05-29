42ad523c3ec34388f5d71c5687824bc8d9990d00
JaySire <josiah5115@gmail.com>
2018-05-28 23:09:35 -0600

    Container Componnts Defined

--- public/app.jsx
+++ public/app.jsx
@@ -97,5 +97,9 @@ ReactDOM.render(
 // 2). Get Initial State, sets its name state to the Props value
 // 3). Down in the render func we use this.dot name to fetch (we initialize and store it in the name variable and call it down in the 'ref' of the form input section) the value and we display it to the screen using the form and input functionalities.
 // 4). When a user submits the form, we call this dot state which updates the state and re-render the parts of the Component that matter.
-// Presentational Components: They render out something visible on the computer screen, or sometimes they can be set up where they respond to user input. e.g we have a form with an input field and a button gets clicked.
+// Presentational Components: (Will be blue and green in our case). They render out something visible on the computer screen, or sometimes they can be set up where they respond to user input. e.g we have a form with an input field and a button gets clicked.
 // Presentational Comps do not maintain state, they're dummy comps; they simply render stuff to the browser and call functions when a user interacts with content.
+// Container Component: (Will be the red one in our case). They DO maintain State. And in our case, the r'ed compo' will maintain the 'name' State. When it gets changed by the blue compo it will re-render the green compo. because it automatically re-renders children when it's state changes and the children rely on it.
+
+// Presentational Components- render something to the browser
+// Container Components - Maintain State and render children/nested components
