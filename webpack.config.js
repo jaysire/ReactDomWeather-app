cf4a492730b9bbef9db40d2092a3823b030c171b
JaySire <josiah5115@gmail.com>
2018-05-30 19:00:37 -0600

    babel-loader-explained

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,17 @@ module.exports = {
     extensions: ['.js', '.jsx', '']
   },
   // We use bable-loader(a div dependancy - we remove div so we need a loader to convert jsx code to a language our browser can read/convert all our jsx files into ES5 code that we can use today).
-  // Initiate & set the props for our bable-loader: We specify: loader name; query: sets an obj array with an inbuild method - 'presets'; finally we have test property.
+  // Initiate & set the props for our bable-loader: We specify: loader name; query: sets an obj array with an inbuild method - 'presets' that tell the bable loader what to do; finally we have test property.
   module: {
     loaders: [
       {
         // telling bable-loader to take our files and run them thru react & es2015 & get the output.
         loader: 'babel-loader',
         query: {
+          // Presets is an inbuilt method that tells babel-loader what to do.- in our case we asked babel-loader to compile our jsx code into react and es2015.
           presets: ['react', 'es2015']
         },
-        // test attribut to specify what files to look for that match the .jsx attribute.
+        // test attribut to specify what files to look for that match the .jsx attribute. i.e.Which files to apply the loader to. In our case we want all files ending with the jsx extension.
         test: /\.jsx?$/,
         // We specify what files to exclude using this code.
         exclude: /(node_modules|bower_components)/
