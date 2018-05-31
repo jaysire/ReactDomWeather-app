94aeb8facfc33b3621465ef4f44e382d76c6cc64
JaySire <josiah5115@gmail.com>
2018-05-31 09:36:11 -0600

    react-weather-app-initial-set-up

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+  <h1>Hello Salt Lake City! <br/> What a beatiful sunny morning to be alive!</h1>,
+  document.getElementById('app')
+)
