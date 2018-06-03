0885c49f8e3ec81f45df0d89fec069316bd5b8bb
JaySire <josiah5115@gmail.com>
2018-06-03 13:52:28 -0600

    promise function

--- eg-promise.js
+++ eg-promise.js
@@ -11,3 +11,22 @@ getTempCallback('Philadelphia', (err, temp) => {
   }
 });
 
+function getTempPromise(location) {
+  return new Promise(((resolve, reject) => {
+    setTimeout(() => {
+      resolve(79);
+      reject('City not found');
+    });
+  }));
+}
+
+getTempPromise('Philadelphia').then((temp) => {
+  console.log('Promise success', temp)
+}, (err) => {
+  console.log('Promise error', err);
+});
+
+// The first Promise function returns a 'new Promise instance' which takes two parameters: resolve and reject.
+// this ensures we can't have both instancese of successs & error/ failutre, happening at the same time as it was in the case with the getTempCallback. (two codes dont get fired at the same time)
+// then is a Promise inbuit method that takes two functions which each take one parameter.
+//  We fake delay/ simulate delay using setTimeout and it takes a function to run.
