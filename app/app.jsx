0eab2e7f3ae575cda83c17a784785cce9b43b81c
JaySire <josiah5115@gmail.com>
2018-06-02 21:32:24 -0600

    minimal updates

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const {Route, Router, IndexRoute, hashHistory} = require('react-router');
+const {
+  Route, Router, IndexRoute, hashHistory,
+} = require('react-router');
 const Main = require('Main');
 const Weather = require('Weather');
 const About = require('About');
@@ -9,10 +11,10 @@ const Examples = require('Examples');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="about" component={About}/>
-      <Route path="examples" component={Examples}/>
-      <IndexRoute component={Weather}/>
+      <Route path="about" component={About} />
+      <Route path="examples" component={Examples} />
+      <IndexRoute component={Weather} />
     </Route>
   </Router>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
