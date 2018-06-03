4d82ff0be0c0b61cd807ff24bf6d47b5e0c89a38
JaySire <josiah5115@gmail.com>
2018-06-03 15:03:05 -0600

    promise challange

--- eg-promise.js
+++ eg-promise.js
@@ -1,37 +1,60 @@
-function getTempCallback(location, callback) {
-  callback(undefined, 78);
-  callback('City not found');
+// function getTempCallback(location, callback) {
+//   callback(undefined, 78);
+//   callback('City not found');
+// }
+
+// getTempCallback('Philadelphia', (err, temp) => {
+//   if (err) {
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp);
+//   }
+
+//   function getTempPromise(location) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(79);
+//         reject('City not found');
+//       }, 1000);
+//     });
+//   }
+
+//   getTempPromise('Philadelphia').then((temp) => {
+//     console.log('Promise success', temp);
+//   }, (err) => {
+//     console.log('Promise error', err);
+//   });
+// });
+
+
+function addPromise(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      resolve(a + b);
+    } else {
+      reject('a & b NOT numbers!!');
+    }
+  });
 }
 
-getTempCallback('Philadelphia', (err, temp) => {
-  if (err) {
-    console.log('error', err);
-  } else {
-    console.log('success', temp);
-  }
-
-  function getTempPromise(location) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(79);
-        reject('City not found');
-      }, 1000);
-    });
-  }
-
-  getTempPromise('Philadelphia').then((temp) => {
-    console.log('Promise success', temp);
-  }, (err) => {
-    console.log('Promise error', err);
-  });
+// the .then method takes in two parameter that are functions. i.e callback functions.
+addPromise(5, 5).then((sum) => {
+  console.log('success', sum);
+}, (err) => {
+  console.log('error', err);
 });
 
+addPromise('josiah', 9).then(function (sum) {
+  console.log('this should not show up');
+}, function (err) {
+  console.log('WARNING!! ERROR!! CANNOT COMPUTE!!', err);
+});
 
-// The first Promise function returns a 'new Promise instance' which takes two parameters: resolve and reject.
-// this ensures we can't have both instancese of successs & error/ failutre, happening at the same time as it was in the case with the getTempCallback. (two codes dont get fired at the same time).
-// Eventhough resolve & reject were both called the Promise can ONLY complete once/ only run instance at a time. i.e either reject or resolve, but NOT BOTH at the same time or one instance called twice.
-// then is a Promise inbuit method that takes two functions which each take one parameter.
+// // The first Promise function returns a 'new Promise instance' which takes two parameters: resolve and reject.
+// // this ensures we can't have both instancese of successs & error/ failutre, happening at the same time as it was in the case with the getTempCallback. (two codes dont get fired at the same time).
+// // Eventhough resolve & reject were both called the Promise can ONLY complete once/ only run instance at a time. i.e either reject or resolve, but NOT BOTH at the same time or one instance called twice.
+// // then is a Promise inbuit method that takes two functions which each take one parameter.
 
-// NOTE: Since in these cases  we resolve or reject or call the callback right away, thus eliminating the need for using callback or Promise. Thus we fake the delay using setTimeout.
-//  We fake/ simulate delay using setTimeout and it takes a function to run.
-// 1000 miliseconds = 1 second.
+// // NOTE: Since in these cases  we resolve or reject or call the callback right away, thus eliminating the need for using callback or Promise. Thus we fake the delay using setTimeout.
+// //  We fake/ simulate delay using setTimeout and it takes a function to run.
+// // 1000 miliseconds = 1 second.
