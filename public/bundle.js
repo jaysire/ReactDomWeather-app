916152e72613461ca117e4a456f4e208381360b2
JaySire <josiah5115@gmail.com>
2018-06-04 20:18:05 -0600

    update About Page

--- public/bundle.js
+++ public/bundle.js
@@ -26750,9 +26750,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component!'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to our about page!'
+	    )
 	  );
 	};
 
