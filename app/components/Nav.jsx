1098c8c960afb581112cf37b2f98b5afe9d25c70
JaySire <josiah5115@gmail.com>
2018-06-05 21:35:39 -0600

    changes after roll-back

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,7 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
       e.preventDefault();
-      alert('Not yet wired up!');
+      alert('Moved on to Redux, not yet wired up!');
   },
   render: function () {
     return (
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -31,6 +31,14 @@ var Weather = React.createClass({
       });
     });
   },
+  componentDidMount() {
+    const location = this.props.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+    }
+  },
+  
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
 
